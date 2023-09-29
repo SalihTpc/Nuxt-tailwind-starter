d1166c7b9174d9c2e029452be48949bd45f483a8
SalihTpc <salihtopcu@hotmail.com.tr>
2023-09-29 14:24:52 +0300

    first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,16 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+import { resolve } from "path";
+
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+  css: ["~/assets/main.scss"],
+  alias: {
+    "@": resolve(__dirname, "/"),
+  },
+});
